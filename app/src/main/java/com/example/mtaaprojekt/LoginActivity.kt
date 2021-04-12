d263e08b88a2b0b9b48311adbd35046dfca16f25
MatejKubena <kubenamatej6@gmail.com>
2021-04-12 17:20:12 +0200

    Activity switch

---
+++ app/src/main/java/com/example/mtaaprojekt/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mtaaprojekt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mtaaprojekt.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+//            val mail = binding.editTextTextEmailAddress.text
+//            val password = binding.editTextTextPassword.text
+//
+//            val url = "http://localhost:8080/login"
+
+            val goToProfile = Intent(this, ProfileActivity::class.java)
+            startActivity(goToProfile)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mtaaprojekt/ProfileActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mtaaprojekt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mtaaprojekt.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
